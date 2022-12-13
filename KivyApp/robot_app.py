3e1d2ea814326d170cdab65dffdec8926cd410c7
Rafael Pimenta <jrafapimenta@gmail.com>
2022-12-13 01:04:05 +0000

    kivy app complete, debug while true nodemuc

--- KivyApp/robot_app.py
+++ KivyApp/robot_app.py
@@ -1,12 +1,20 @@
 from kivymd.app import MDApp as App
 from kivymd.theming import ThemeManager
 from kivymd.uix.widget import Widget
+from kivy.core.window import Window
 from kivy.lang import Builder
+import requests
 
-Builder.load_file('robot_app.kv')
 
 class RobotAppLayout(Widget):
-    pass
+    def send_comand(self, command:str):
+        link = "http://127.0.0.1:5000/robot-com"
+        data = {
+            "nome":command,
+            "estado":True
+        }
+
+        r = requests.patch(link, json = data)
 
 class RobotApp(App):
     def build(self):
@@ -17,4 +25,7 @@ class RobotApp(App):
         return RobotAppLayout()
 
 if __name__ == '__main__':
+    Builder.load_file('robot_app.kv')
+    Window.size = (500, 700)
+
     RobotApp().run()
